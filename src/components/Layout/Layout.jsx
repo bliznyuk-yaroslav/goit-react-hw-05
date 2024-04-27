bc5e7445776a58390349d761bd5efd3a0f230827
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-04-27 21:06:58 +0300

    prav

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 import { Suspense } from "react";
 import css from "./Layout.module.css";
 import Navigation from "../Navigation/Navigation";
+import Loader from '../Loader/Loader'
 
 
 export default function Layout({ children }) {
     return (
       <div className={css.container}>
         <Navigation />
-        <Suspense fallback={<div>Please wait loading page...</div>}>
+        <Suspense fallback={<Loader/>}>
           {children}
         </Suspense>
       </div>
